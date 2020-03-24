07627f091c889851d654d5340ea92ce93c76de35
Gonzalo Moreno <gonzamv@gmail.com>
2020-03-24 16:11:36 +0100

    password forget and reset

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
-const Account = () => (
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
+const AccountPage = () => (
   <div>
-    <h1>Account</h1>
+    <h1>Account Page</h1>
+
+    <h3>Password Forget</h3>
+    <PasswordForgetForm />
+    <h3>Password Change</h3>
+    <PasswordChangeForm />
   </div>
 );
-export default Account;
+export default AccountPage;
